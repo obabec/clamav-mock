f5ed4d33ce34fb9cf3ecdce13d7c6056c98d046e
Ondrej Babec <obabec@dnanexus.com>
2025-10-15 13:25:16 +0200

    feat: Support also not session requests

--- app.rb
+++ app.rb
@@ -9,13 +9,45 @@ port = ENV.fetch("CLAMD_TCP_PORT", nil).to_i
 port = 3310 if port.zero?
 
 EICAR = 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
-# optionally padded to with whitespace to 127 characters https://www.eicar.org/?page_id=3950
+# optionally padded with whitespace to 127 characters https://www.eicar.org/?page_id=3950
 EICAR_REGEXP = /\A#{Regexp.escape(EICAR)}[ \t\n\r\x1a]{0,60}\z/
 EICAR_NAME = "Win.Test.EICAR_HDB-1"
 EICAR_LEGACY_NAME = "Eicar-Signature"
 ZIP_MAGIC = "\x50\x4B\x03\x04".b
 OLE_MAGIC = "\xD0\xCF\x11\xE0".b
 MAX_SIZE = 1024 * 100 # 100kb
+
+# -------- helpers --------
+def read_until(io, delimiter)
+  buf = +""
+  while (ch = io.getc)
+    break if ch == delimiter
+    buf << ch
+  end
+  ch.nil? ? nil : buf
+end
+
+def read_line_crlf(io)
+  line = read_until(io, "\n")
+  return nil if line.nil?
+  line.end_with?("\r") ? line[0..-2] : line
+end
+
+def read_exact(io, n)
+  data = +""
+  while data.bytesize < n
+    chunk = io.read(n - data.bytesize)
+    return nil if chunk.nil? || chunk.empty?
+    data << chunk
+  end
+  data
+end
+
+def respond(client, delimiter, session_id, body)
+  prefix = session_id ? "#{session_id}: " : ""
+  client.write("#{prefix}#{body}#{delimiter}")
+end
+# -------------------------
 
 class ScanIO < StringIO
   def self.new(...)
@@ -27,7 +59,6 @@ class ScanIO < StringIO
   def write(data, *)
     # Skip writing if we have enough data
     return data.length if max_size?
-
     super
   end
 
@@ -54,7 +85,6 @@ class ScanIO < StringIO
     # Allow one extra byte for oversized eicar
     return false if string.length <= 128
     return string.length > MAX_SIZE if zip? || ole?
-
     string.length > 128
   end
 
@@ -67,20 +97,15 @@ class ScanIO < StringIO
   end
 
   def ole_eicar
-    # This is to support a particular docx eicar file
-    # The eicar is is in an OLENativeStream within the OLE container
-    # While you can use ruby-ole to parse the OLE container
-    # I can't figure out the OLENativeStream
-    # So, we'll just call it the virus if the string is anywhere in the OLE container
+    # Support for a particular docx eicar file
     string.include?(EICAR) ? EICAR_NAME : false
   end
 
   def zip_eicar
-    # This doesn't work for some zips, but it is fine for the example eicar files
+    # Works for the example eicar zips
     stream = Zip::InputStream.new(self)
     while (entry = stream.get_next_entry)
       next if entry.size > MAX_SIZE
-
       result = check_zip_stream(entry)
       return result unless result == false
     end
@@ -97,57 +122,90 @@ class ScanIO < StringIO
 end
 
 current_id = 0
-
 server = TCPServer.new port
 
 loop do
   Thread.start(server.accept) do |client|
-    # https://linux.die.net/man/8/clamd
-    session = ""
-    loop do
-      # Commands start with z or n, which specifies the delimiter for both input and output
-      prefix = client.getc
-      break if prefix.nil?
+    session_id = nil # nil = non-session; Integer when in IDSESSION
+    begin
+      loop do
+        first = client.getc
+        break if first.nil?
 
-      delimiter = case prefix
-                  when "z" then "\0"
-                  when "n" then "\n"
-                  else
-                    raise "unexpected message prefix #{prefix}"
-                  end
-
-      command = ""
-      while (char = client.getc) != delimiter
-        command += char
-      end
-
-      case command
-      when "IDSESSION"
-        # Start a session. Assign a number
-        session = "#{current_id + 1}: "
-        current_id += 1
-      when "END"
-        # End a session
-        break
-      when "PING"
-        client.write("#{session}PONG#{delimiter}")
-      when "INSTREAM"
-        io = ScanIO.new
-        # Chunks are size, followed by the bytes
-        loop do
-          size = client.read(4).unpack1("N")
-          break if size.zero?
-
-          io.write client.read(size)
+        # Accept prefixed (z/n) and bare commands
+        if first == "z" || first == "n"
+          delimiter = (first == "z") ? "\0" : "\n"
+          command = (delimiter == "\n") ? read_line_crlf(client) : read_until(client, delimiter)
+          break if command.nil?
+        else
+          delimiter = "\n"
+          rest = read_line_crlf(client)
+          break if rest.nil?
+          command = first + rest
         end
-        client.write(io.virus? ? "#{session}stream: #{io.virus_name} FOUND#{delimiter}" : "#{session}stream: OK#{delimiter}")
-      end
 
-      break unless session
+        case command
+        when "IDSESSION"
+          current_id += 1
+          session_id = current_id
+
+        when "END", "QUIT"
+          break
+
+        when "PING"
+          respond(client, delimiter, session_id, "PONG")
+
+        when "VERSION"
+          # Minimal stub
+          respond(client, delimiter, session_id, "ClamAV mock 0.0")
+
+        when "INSTREAM"
+          io = ScanIO.new
+          total = 0
+          size_limit_exceeded = false
+
+          loop do
+            size_bytes = read_exact(client, 4)
+            break if size_bytes.nil?
+            size = size_bytes.unpack1("N")
+            break if size.zero?
+
+            chunk = read_exact(client, size)
+            break if chunk.nil?
+
+            total += chunk.bytesize
+            if total > MAX_SIZE
+              size_limit_exceeded = true
+              # drain to 0-size terminator
+              loop do
+                sb = read_exact(client, 4); break if sb.nil?
+                s = sb.unpack1("N"); break if s.zero?
+                break if read_exact(client, s).nil?
+              end
+              break
+            else
+              io.write chunk
+            end
+          end
+
+          if size_limit_exceeded
+            respond(client, delimiter, session_id, "INSTREAM size limit exceeded. ERROR")
+          else
+            msg = io.virus? ? "stream: #{io.virus_name} FOUND" : "stream: OK"
+            respond(client, delimiter, session_id, msg)
+          end
+
+          # Non-session single shot: close after reply so clients exit cleanly
+          break if session_id.nil?
+
+        else
+          respond(client, delimiter, session_id, "UNKNOWN COMMAND")
+        end
+      end
+    rescue StandardError => e
+      warn e.full_message
+    ensure
+      client.close
     end
-  rescue StandardError => e
-    warn e.full_message
-  ensure
-    client.close
   end
 end
